17d26379a3fdb3ac0bfb41780600621404edfad5
Dave <gtfjet@gmail.com>
2021-08-27 18:50:04 -0400

    Update ex6.js

--- ex6.js
+++ ex6.js
@@ -1,38 +1,39 @@
 let svgNS = "http://www.w3.org/2000/svg";
 let diagram = document.createElementNS(svgNS, 'svg');
-diagram.setAttribute('width', 1000);
-diagram.setAttribute('height', 1000);
+diagram.setAttribute('width', 600);
+diagram.setAttribute('height', 900);
+diagram.setAttribute('viewBox', "0 0 600 900");
 document.body.appendChild(diagram);
 var solver = new Solver();
 
 let mass1 = new Mass(2);
 mass1.x  = 100;
-mass1.y  = 100;
+mass1.y  = 200;
 // mass1.vy = -300;
 
 let mass2 = new Mass(2);
 mass2.x  = 200;
-mass2.y  = 100;
+mass2.y  = 200;
 
 let mass3 = new Mass(2);
 mass3.x  = 300;
-mass3.y  = 100;
+mass3.y  = 200;
 
 let mass4 = new Mass(2);
 mass4.x  = 400;
-mass4.y  = 100;
+mass4.y  = 200;
 
 let mass5 = new Mass(2);
 mass5.x  = 500;
-mass5.y  = 100;
+mass5.y  = 200;
 
 let mount1 = new Mount();
 mount1.flange.x = 300;
-mount1.flange.y = 100;
+mount1.flange.y = 200;
 
 let mount2 = new Mount();
 mount2.flange.x = 300;
-mount2.flange.y = 400;
+mount2.flange.y = 500;
 
 let spring1 = new Spring();
 let spring2 = new Spring();
@@ -60,4 +61,3 @@ function run() {
 }
 // run();
 setInterval(run,solver.timeStep*1000);
-
