cac49cc32f949e8afc8a79ef344b7807bd37c1f3
Dave <gtfjet@gmail.com>
2021-09-03 10:55:18 -0400

    Update ex6.js

--- ex6.js
+++ ex6.js
@@ -49,35 +49,7 @@ spring6.flange_b = mount2.flange;
 
 function run() {
   if(!paused) {
-    var x1=mass1.x;
-    var y1=mass1.y;
-    var x2=mass2.x;
-    var y2=mass2.y;
-    var x4=mass4.x;
-    var y4=mass4.y;
-    var x5=mass5.x;
-    var y5=mass5.y;
     solver.run();
-    c.strokeStyle = mass1.color;
-    c.beginPath();
-    c.moveTo(x1,y1);
-    c.lineTo(mass1.x,mass1.y);
-    c.stroke();
-    c.strokeStyle = mass2.color;
-    c.beginPath();
-    c.moveTo(x2,y2);
-    c.lineTo(mass2.x,mass2.y);
-    c.stroke();
-    c.strokeStyle = mass4.color;
-    c.beginPath();
-    c.moveTo(x4,y4);
-    c.lineTo(mass4.x,mass4.y);
-    c.stroke();
-    c.strokeStyle = mass5.color;
-    c.beginPath();
-    c.moveTo(x5,y5);
-    c.lineTo(mass5.x,mass5.y);
-    c.stroke();
   }
 }
 solver.maxIterations=1;
